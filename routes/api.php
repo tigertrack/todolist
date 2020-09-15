9c7026cefe063ff15b3004b5a1cc5e9ca753a0c7
tigertrack <riandyfadly@gmail.com>
2020-09-15 18:13:05 +0700

    added section endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/sections', 'SectionController@index');
+Route::get('/section/{section}', 'SectionController@show');
+Route::post('/section', 'SectionController@store');
+Route::patch('/section/{section}', 'SectionController@update');
+Route::delete('/section/{section}', 'SectionController@destroy');
+
